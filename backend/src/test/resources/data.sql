9cbee634d7762962894842f3b528c265735f896a
Joao-Vitor-Rd <joao789roye@gmail.com>
2026-01-18 22:25:30 -0300

    test: retirada dos migrantion

---
+++ backend/src/test/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO game (developer, name, year, star_rating, finished) 
+VALUES 
+('Nintendo','The Legend of Zelda: Breath of the Wild',2017,5,TRUE),
+('FromSoftware','Elden Ring',2022,5,FALSE),
+('CD Projekt Red','Cyberpunk 2077',2020,4,FALSE),
+('CD Projekt Red','The Witcher 3: Wild Hunt',2015,5,TRUE),
+('FromSoftware','Dark Souls III',2016,5,TRUE),
+('Team Cherry','Hollow Knight',2017,5,FALSE),
+('Supergiant Games','Hades',2020,5,TRUE),
+('ConcernedApe','Stardew Valley',2016,5,TRUE);
